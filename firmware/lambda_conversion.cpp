a61157760ae47246d4db4c4e06f94d00931ff169
Matthew Kennedy <matthewkennedy@outlook.com>
2020-11-02 01:28:30 -0800

    fix lambda math

--- firmware/lambda_conversion.cpp
+++ firmware/lambda_conversion.cpp
@@ -6,7 +6,7 @@ static float GetPhi(float pumpCurrent)
 {
     // This estimation is accurate within 0.5% from 0.8 to 1.0, and 0.01% from 1 to 1.2 lambda when compared to the lookup table in the Bosch datasheet
     // This error is less than half of the claimed accuracy of the sensor itself
-    float gain = pumpCurrent > 0 ? -0.28299f : -0.44817f;
+    float gain = pumpCurrent < 0 ? -0.28299f : -0.44817f;
 
     return gain * pumpCurrent + 0.99559f;
 }
