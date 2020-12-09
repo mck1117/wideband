0e0281f372d01f71374050fb9adcb497e9645341
Matthew Kennedy <makenne@microsoft.com>
2020-12-08 23:40:32 -0800

    bl build

--- firmware/bootloader/build_bootloader.sh
+++ firmware/bootloader/build_bootloader.sh
@@ -5,6 +5,6 @@ make clean && make -j4
 
 # convert bootloader bin output to a linkable .o file
 # first pad the binary out with 0xFF
-arm-none-eabi-objcopy -I binary -O binary --gap-fill 0xFF --pad-to 0x2800 build/wideband_bootloader.bin build/wideband_bootloader.padded.bin
+arm-none-eabi-objcopy -I binary -O binary --gap-fill 0xFF --pad-to 0x1000 build/wideband_bootloader.bin build/wideband_bootloader.padded.bin
 # then convert it to a .o file
 arm-none-eabi-objcopy -I binary -O elf32-littlearm -B arm --rename-section .data=.bl build/wideband_bootloader.padded.bin build/wideband_bootloader.o
