7d96535419f089e96cf74528c40b80e0851752fd
Matthew Kennedy <makenne@microsoft.com>
2020-12-14 15:46:54 -0800

    update build script

--- firmware/build_wideband.sh
+++ firmware/build_wideband.sh
@@ -9,6 +9,7 @@ cd ..
 rm -r build/
 rm ../for_rusefi/wideband_image.h
 
+# build app firmware!
 make -j12
 
 # Copy the bin without the bootloader (the image consumed by rusEfi has no bootloader on it)
@@ -17,11 +18,14 @@ dd if=build/wideband.bin of=build/wideband_noboot_no_pad.bin skip=6144 bs=1
 # extend the image out to full size (32k (flash) - 6k (bootloader) - 1k (config) - 4 (crc) = 25k - 4 = 0x63FC)
 arm-none-eabi-objcopy -I binary -O binary --gap-fill 0xFF --pad-to 0x63FC build/wideband_noboot_no_pad.bin build/wideband_fullsize_nocrc.bin
 
-# compute the crc and write that to a file
+# compute the crc and write that to a file (in binary)
 crc32 build/wideband_fullsize_nocrc.bin | xxd -r -p - > build/wideband_crc.bin
 
 # Now glue the image and CRC together
 cat build/wideband_fullsize_nocrc.bin build/wideband_crc.bin > build/wideband_image.bin
+
+# For good measure build a complete bin that includes the bootloader
+cat bootloader/build/wideband_bootloader.padded.bin build/wideband_image.bin > ../for_rusefi/wideband_image_with_bl.bin
 
 # Convert to a header file, and tack "static const " on the front of it
 xxd -i build/wideband_image.bin \
