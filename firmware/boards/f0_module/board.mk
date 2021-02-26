cdd4a60fdc99e8b2c2fcbdfdb6fcd45538e321c6
Matthew Kennedy <makenne@microsoft.com>
2021-02-25 22:26:55 -0800

    grumble grumble this changes the link order

--- firmware/boards/f0_module/board.mk
+++ firmware/boards/f0_module/board.mk
@@ -1,1 +1,5 @@
 USE_BOOTLOADER = yes
+
+include $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/mk/startup_stm32f0xx.mk
+include $(CHIBIOS)/os/hal/ports/STM32/STM32F0xx/platform.mk
+include $(CHIBIOS)/os/common/ports/ARMv6-M/compilers/GCC/mk/port.mk
