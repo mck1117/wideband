b2b05b0c45c8137ebe8777d64262b75de13bff1a
Matthew Kennedy <makenne@microsoft.com>
2021-02-25 22:35:26 -0800

    extract mcu

--- firmware/boards/f0_module/board.mk
+++ firmware/boards/f0_module/board.mk
@@ -1,5 +1,7 @@
 USE_BOOTLOADER = yes
 
+MCU = cortex-m0
+
 include $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/mk/startup_stm32f0xx.mk
 include $(CHIBIOS)/os/hal/ports/STM32/STM32F0xx/platform.mk
 include $(CHIBIOS)/os/common/ports/ARMv6-M/compilers/GCC/mk/port.mk
