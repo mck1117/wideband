b0763f3023d1c622036d34dec5475f6b4782b973
Matthew Kennedy <makenne@microsoft.com>
2021-02-25 22:59:02 -0800

    dave CAN

--- firmware/boards/f1_dave/port.cpp
+++ firmware/boards/f1_dave/port.cpp
@@ -5,3 +5,9 @@ AnalogResult AnalogSample()
     // TODO: implement me!
     return {};
 }
+
+const CANConfig canConfig500 =
+{
+    CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP | CAN_MCR_NART,
+    0 // TODO: set bit timing! correctly!
+};
