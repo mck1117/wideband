17bf809a5f4b6d24fcb601ca765a74faa5d554cd
Matthew Kennedy <matthewkennedy@outlook.com>
2021-12-27 20:35:45 -0800

    io pins f1

---
+++ firmware/boards/f1_dave/io/io_pins.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// TODO: is this correct?
+
+#define BLUE_LED_PORT GPIOB
+#define BLUE_LED_PIN 5
+
+#define GREEN_LED_PORT GPIOB
+#define GREEN_LED_PIN 6
+
+#define NERNST_ESR_DRIVER_PORT GPIOB
+#define NERNST_ESR_DRIVER_PIN 7
