2849b984dd8ac0e0bf9d6958ab65e7c5b001c543
Matthew Kennedy <matthewkennedy@outlook.com>
2022-01-28 11:21:38 -0800

    some per board config

--- firmware/wideband_config.h
+++ firmware/wideband_config.h
@@ -1,17 +1,11 @@
 #pragma once
 
 #include "io_pins.h"
-
-// Fundamental board constants
-#define VCC_VOLTS (3.3f)
-#define HALF_VCC (VCC_VOLTS / 2)
-#define ADC_MAX_COUNT (4095)
-#define ADC_OVERSAMPLE 24
+#include "wideband_board_config.h"
 
 // *******************************
 //    Nernst voltage & ESR sense
 // *******************************
-#define NERNST_INPUT_GAIN (1 / 2.7f)
 
 // Nernst AC injection resistor value
 #define ESR_SUPPLY_R (22000)
