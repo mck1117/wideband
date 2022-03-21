81cb9a0503da5c68c363eef5766346cbf1fb0df6
Andrey Gusakov <dron0gus@gmail.com>
2022-03-22 00:31:04 +0300

    Smarter state machine to use internal vbatt when CAN not connected

--- firmware/wideband_config.h
+++ firmware/wideband_config.h
@@ -40,6 +40,12 @@
 #define HEATER_PREHEAT_TIME 5000
 #define HEATER_WARMUP_TIMEOUT 60000
 
+#define HEATER_BATTERY_STAB_TIME	3000
+/* minimal battery voltage to start heating without CAN command */
+#define HEATER_BATTERY_ON_VOLTAGE	9.5
+/* mininal battery voltage to continue heating */
+#define HEATER_BATTETY_OFF_VOLTAGE  8.5
+
 #define HEATER_CLOSED_LOOP_THRESHOLD_ESR 500
 #define HEATER_TARGET_ESR 300
 #define HEATER_OVERHEAT_ESR 150
