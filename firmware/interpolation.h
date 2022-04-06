fdb024c4eb2584e46fc51a84f99bc165224265ef
Andrey Gusakov <dron0gus@gmail.com>
2022-04-06 13:18:06 +0300

    integer 1D interpolation

---
+++ firmware/interpolation.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct inter_point {
+	int		x;
+	int		y;
+};
+
+int interpolateInt(int x1, int y1, int x2, int y2, int x);
+int interpolateIntClamped(int x1, int y1, int x2, int y2, int x);
+int interpolate_1d_int(const struct inter_point *p, int size, int x);
