354888f2b815c48a99f5271421b9c25c5b2cb4d0
Matthew Kennedy <matthewkennedy@outlook.com>
2022-04-06 01:31:35 -0700

    comments

--- firmware/can.h
+++ firmware/can.h
@@ -7,8 +7,13 @@ void SendCanData(float lambda, uint16_t measuredResistance);
 void SendRusefiFormat(uint8_t idx);
 
 enum class HeaterAllow {
+    // no CAN message telling us what to do has been rx'd
     Unknown,
+
+    // We got a message, it said HEAT
     Allowed,
+
+    // We got a message, it said NO HEAT
     NotAllowed,
 };
 
