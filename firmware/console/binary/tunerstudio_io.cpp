613db5a43e04abc650fa974b3148691c63fc4599
Andrey Gusakov <dron0gus@gmail.com>
2022-05-15 05:31:39 -0400

    Steal tunerstudio files from RusEFI
    (cherry picked from commit dcba04fbd31700f5ca4ef4f12a6e220c5713c7d0)

---
+++ firmware/console/binary/tunerstudio_io.cpp
@@ -0,0 +1,126 @@
+/**
+ * @file	tunerstudio_io.cpp
+ *
+ * @date Mar 8, 2015
+ * @author Andrey Belomutskiy, (c) 2012-2020
+ */
+
+#include "pch.h"
+#include "os_access.h"
+#include "tunerstudio_io.h"
+
+#if EFI_SIMULATOR
+#include "rusEfiFunctionalTest.h"
+#endif // EFI_SIMULATOR
+
+#if EFI_PROD_CODE || EFI_SIMULATOR
+size_t TsChannelBase::read(uint8_t* buffer, size_t size) {
+	return readTimeout(buffer, size, SR5_READ_TIMEOUT);
+}
+#endif
+
+#define isBigPacket(size) ((size) > BLOCKING_FACTOR + 7)
+
+void TsChannelBase::copyAndWriteSmallCrcPacket(uint8_t responseCode, const uint8_t* buf, size_t size) {
+	auto scratchBuffer = this->scratchBuffer;
+
+	// don't transmit too large a buffer
+	efiAssertVoid(OBD_PCM_Processor_Fault, !isBigPacket(size), "copyAndWriteSmallCrcPacket tried to transmit too large a packet")
+
+	// If transmitting data, copy it in to place in the scratch buffer
+	// We want to prevent the data changing itself (higher priority threads could write
+	// tsOutputChannels) during the CRC computation.  Instead compute the CRC on our
+	// local buffer that nobody else will write.
+	if (size) {
+		memcpy(scratchBuffer + 3, buf, size);
+	}
+
+	crcAndWriteBuffer(responseCode, size);
+}
+
+void TsChannelBase::crcAndWriteBuffer(uint8_t responseCode, size_t size) {
+	efiAssertVoid(OBD_PCM_Processor_Fault, !isBigPacket(size), "crcAndWriteBuffer tried to transmit too large a packet")
+
+	auto scratchBuffer = this->scratchBuffer;
+	// Index 0/1 = packet size (big endian)
+	*(uint16_t*)scratchBuffer = SWAP_UINT16(size + 1);
+	// Index 2 = response code
+	scratchBuffer[2] = responseCode;
+
+	// CRC is computed on the responseCode and payload but not length
+	uint32_t crc = crc32(&scratchBuffer[2], size + 1); // command part of CRC
+
+	// Place the CRC at the end
+	*reinterpret_cast<uint32_t*>(&scratchBuffer[size + 3]) = SWAP_UINT32(crc);
+
+	// Write to the underlying stream
+	write(reinterpret_cast<uint8_t*>(scratchBuffer), size + 7, true);
+	flush();
+}
+
+void TsChannelBase::writeCrcPacketLarge(uint8_t responseCode, const uint8_t* buf, size_t size) {
+	uint8_t headerBuffer[3];
+	uint8_t crcBuffer[4];
+
+	*(uint16_t*)headerBuffer = SWAP_UINT16(size + 1);
+	*(uint8_t*)(headerBuffer + 2) = responseCode;
+
+	// Command part of CRC
+	uint32_t crc = crc32((void*)(headerBuffer + 2), 1);
+	// Data part of CRC
+	crc = crc32inc((void*)buf, crc, size);
+	*(uint32_t*)crcBuffer = SWAP_UINT32(crc);
+
+	// Write header
+	write(headerBuffer, sizeof(headerBuffer), false);
+
+	// If data, write that
+	if (size) {
+		write(buf, size, false);
+	}
+
+	// Lastly the CRC footer
+	write(crcBuffer, sizeof(crcBuffer), true);
+	flush();
+}
+
+TsChannelBase::TsChannelBase(const char *name) {
+	this->name = name;
+}
+
+void TsChannelBase::assertPacketSize(size_t size, bool allowLongPackets) {
+	if (isBigPacket(size) && !allowLongPackets) {
+		firmwareError(OBD_PCM_Processor_Fault, "tried to send disallowed long packet of size %d", size);
+	}
+}
+
+/**
+ * Adds size to the beginning of a packet and a crc32 at the end. Then send the packet.
+ */
+void TsChannelBase::writeCrcPacket(uint8_t responseCode, const uint8_t* buf, size_t size, bool allowLongPackets) {
+	// don't transmit a null buffer...
+	if (!buf) {
+		size = 0;
+	}
+
+	assertPacketSize(size, allowLongPackets);
+
+	if (isBigPacket(size)) {
+		// for larger packets we do not use a buffer for CRC calculation meaning data is now allowed to modify while pending
+		writeCrcPacketLarge(responseCode, buf, size);
+	} else {
+		// for small packets we use a buffer for CRC calculation
+		copyAndWriteSmallCrcPacket(responseCode, buf, size);
+	}
+}
+
+void TsChannelBase::sendResponse(ts_response_format_e mode, const uint8_t * buffer, int size, bool allowLongPackets /* = false */) {
+	if (mode == TS_CRC) {
+		writeCrcPacket(TS_RESPONSE_OK, buffer, size, allowLongPackets);
+	} else {
+		if (size > 0) {
+			write(buffer, size, true);
+			flush();
+		}
+	}
+}
---
+++ firmware/console/binary/tunerstudio_io.h
@@ -0,0 +1,127 @@
+/**
+ * @file	tunerstudio_io.h
+ * @file TS protocol commands and methods are here
+ *
+ * @date Mar 8, 2015
+ * @author Andrey Belomutskiy, (c) 2012-2020
+ */
+
+#pragma once
+#include "global.h"
+#include "tunerstudio_impl.h"
+
+#if (!TS_NO_PRIMARY && defined(TS_PRIMARY_PORT))
+	#define HAS_PRIMARY true
+#else
+	#define HAS_PRIMARY false
+#endif
+
+#if (!TS_NO_SECONDARY && defined(TS_SECONDARY_PORT))
+	#define HAS_SECONDARY true
+#else
+	#define HAS_SECONDARY false
+#endif
+
+#if EFI_USB_SERIAL
+#include "usbconsole.h"
+#endif // EFI_USB_SERIAL
+
+#if EFI_PROD_CODE
+#include "pin_repository.h"
+#endif
+
+class TsChannelBase {
+public:
+	TsChannelBase(const char *name);
+	// Virtual functions - implement these for your underlying transport
+	virtual void write(const uint8_t* buffer, size_t size, bool isEndOfPacket = false) = 0;
+	virtual size_t readTimeout(uint8_t* buffer, size_t size, int timeout) = 0;
+
+	// These functions are optional to implement, not all channels need them
+	virtual void flush() { }
+	virtual bool isConfigured() const { return true; }
+	virtual bool isReady() const { return true; }
+	virtual void stop() { }
+
+	// Base functions that use the above virtual implementation
+	size_t read(uint8_t* buffer, size_t size);
+
+#ifdef EFI_CAN_SERIAL
+	virtual	// CAN device needs this function to be virtual for small-packet optimization
+#endif
+	void writeCrcPacket(uint8_t responseCode, const uint8_t* buf, size_t size, bool allowLongPackets = false);
+	void sendResponse(ts_response_format_e mode, const uint8_t * buffer, int size, bool allowLongPackets = false);
+
+	/**
+	 * See 'blockingFactor' in rusefi.ini
+	 */
+	char scratchBuffer[BLOCKING_FACTOR + 30];
+	const char *name;
+
+	void assertPacketSize(size_t size, bool allowLongPackets);
+	void crcAndWriteBuffer(uint8_t responseCode, size_t size);
+	void copyAndWriteSmallCrcPacket(uint8_t responseCode, const uint8_t* buf, size_t size);
+
+private:
+	void writeCrcPacketLarge(uint8_t responseCode, const uint8_t* buf, size_t size);
+};
+
+// This class represents a channel for a physical async serial poart
+class SerialTsChannelBase : public TsChannelBase {
+public:
+	SerialTsChannelBase(const char *name) : TsChannelBase(name) {};
+	// Open the serial port with the specified baud rate
+	virtual void start(uint32_t baud) = 0;
+};
+
+#if HAL_USE_SERIAL
+// This class implements a ChibiOS Serial Driver
+class SerialTsChannel : public SerialTsChannelBase {
+public:
+	SerialTsChannel(SerialDriver& driver) : SerialTsChannelBase("Serial"), m_driver(&driver) { }
+
+	void start(uint32_t baud) override;
+	void stop() override;
+
+	void write(const uint8_t* buffer, size_t size, bool isEndOfPacket) override;
+	size_t readTimeout(uint8_t* buffer, size_t size, int timeout) override;
+
+private:
+	SerialDriver* const m_driver;
+};
+#endif // HAL_USE_SERIAL
+
+#if HAL_USE_UART
+// This class implements a ChibiOS UART Driver
+class UartTsChannel : public SerialTsChannelBase {
+public:
+	UartTsChannel(UARTDriver& driver) : SerialTsChannelBase("UART"), m_driver(&driver) { }
+
+	void start(uint32_t baud) override;
+	void stop() override;
+
+	void write(const uint8_t* buffer, size_t size, bool isEndOfPacket) override;
+	size_t readTimeout(uint8_t* buffer, size_t size, int timeout) override;
+
+protected:
+	UARTDriver* const m_driver;
+	UARTConfig m_config;
+};
+#endif // HAL_USE_UART
+
+#define CRC_VALUE_SIZE 4
+// todo: double-check this
+#define CRC_WRAPPING_SIZE (CRC_VALUE_SIZE + 3)
+
+// that's 1 second
+#define BINARY_IO_TIMEOUT TIME_MS2I(1000)
+
+// that's 1 second
+#define SR5_READ_TIMEOUT TIME_MS2I(1000)
+
+void startSerialChannels();
+SerialTsChannelBase* getBluetoothChannel();
+
+void startCanConsole();
+
+void sendOkResponse(TsChannelBase *tsChannel, ts_response_format_e mode);
