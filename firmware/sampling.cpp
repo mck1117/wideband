036c74aa19ecbb8119f348fe09e961d5e8a49acc
Matthew Kennedy <matthewkennedy@outlook.com>
2022-06-29 12:15:02 -0700

    weird merge

--- firmware/sampling.cpp
+++ firmware/sampling.cpp
@@ -42,10 +42,7 @@ static THD_WORKING_AREA(waSamplingThread, 256);
 
 static void SamplingThread(void*)
 {
-    // GD32: Insert delay after enabling ADC
-    chThdSleepMilliseconds(1);
-
-    /* GD32: Insert 20us delay after ADC enable */
+    // GD32: Insert 20us delay after ADC enable
     chThdSleepMilliseconds(1);
 
     while(true)
