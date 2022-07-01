a4d60d19764e5b37dbf56ae66455dae1d4dcb7e3
Andrey Gusakov <dron0gus@gmail.com>
2022-07-01 21:59:49 +0300

    f1_dual: set O0 optimization level due to ADC issue on GD32

--- firmware/boards/f1_dual/build_wideband.sh
+++ firmware/boards/f1_dual/build_wideband.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 #BOARD=f1_dual USE_OPENBLT=yes ../build_f1_board.sh
-BOARD=f1_dual ../build_f1_board.sh
+# set optimization level to 0 until ADC issue is fixed for GD32
+BOARD=f1_dual \
+USE_OPT="-O0 -ggdb -fomit-frame-pointer -falign-functions=16 -fsingle-precision-constant" \../build_f1_board.sh
