64b6fde7fd59dfa4344021b9fa5dcd71dfec0742
Andrey Gusakov <dron0gus@gmail.com>
2022-07-15 00:12:49 +0300

    f1_dual: UART->Serial

--- firmware/boards/f1_dual/halconf.h
+++ firmware/boards/f1_dual/halconf.h
@@ -142,7 +142,7 @@
  * @brief   Enables the SERIAL subsystem.
  */
 #if !defined(HAL_USE_SERIAL) || defined(__DOXYGEN__)
-#define HAL_USE_SERIAL                      FALSE
+#define HAL_USE_SERIAL                      TRUE
 #endif
 
 /**
@@ -177,7 +177,7 @@
  * @brief   Enables the UART subsystem.
  */
 #if !defined(HAL_USE_UART) || defined(__DOXYGEN__)
-#define HAL_USE_UART                        TRUE
+#define HAL_USE_UART                        FALSE
 #endif
 
 /**
