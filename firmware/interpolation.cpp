30eddb27121b4a2f3b9e115de9ad240a3c3eda80
Matthew Kennedy <matthewkennedy@outlook.com>
2022-07-15 16:34:49 -0700

    use shared interpolate2d

--- firmware/interpolation.cpp
+++ firmware/interpolation.cpp
@@ -17,21 +17,3 @@ float interpolateFloatClamped(float x1, float y1, float x2, float y2, float x)
 
 	return interpolateFloat(x1, y1, x2, y2, x);
 }
-
-float interpolate_1d_float(const struct inter_point *p, int size, float x)
-{
-	int i;
-
-	/* no exterpolation */
-	if (x < p[0].x)
-		return p[0].y;
-
-	for (i = 0; i < size - 1; i++) {
-		if ((x >= p[i].x) && (x < p[i + 1].x)) {
-			return interpolateFloat(p[i].x, p[i].y, p[i + 1].x, p[i + 1].y, x);
-		}
-	}
-
-	/* no exterpolation */
-	return p[size - 1].y;
-}
