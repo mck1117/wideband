3b4f6083c2704d27817e6c19a8724d1eeefb5b84
rusefillc <sdfsdfqsf2334234234>
2022-07-18 05:18:48 -0400

    it works!

--- firmware/tools/release.sh
+++ firmware/tools/release.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 BOARD=$1
-DROP=deliver/$BOARD/drop
+FOLDER=rusefi-wideband-$BOARD
+DROP=deliver/$BOARD/$FOLDER
+
+set -e
 
 echo Preparding $BOARD package in $DROP folder
 
+rm -rf $DROP deliver/$BOARD/$BOARD.zip
 mkdir -p $DROP
 
 cp tools/readme.md $DROP
@@ -13,4 +17,10 @@ cp deliver/$BOARD/wideband_update.srec $DROP
 mkdir $DROP/bin
 cp ext/openblt/Host/BootCommander.exe $DROP/bin
 cp ext/openblt/Host/libopenblt.dll $DROP/bin
+cp tools/update-via-pcan.bat $DROP
 
+ls -l $DROP
+
+cd deliver/$BOARD
+zip -r $BOARD.zip $FOLDER
+cd ../..
