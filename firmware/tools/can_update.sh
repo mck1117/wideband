b1e22f20aedcc5a593be1aedc0375e5b0d7c373d
Andrey Gusakov <dron0gus@gmail.com>
2022-07-20 01:43:37 +0300

    tools: can_update: more checks, help

--- firmware/tools/can_update.sh
+++ firmware/tools/can_update.sh
@@ -1,38 +1,60 @@
 #!/bin/bash
 
+help()
+{
+	echo ""
+	echo "Usage:"
+	echo " can_update.sh board [interface]"
+	echo " Default interface is can0"
+	exit -1;
+}
+
 if [ ! -d tools ]; then
 	echo "Please run this script from firmware directory, not tools!"
-	exit -1
+	help
 fi
 
 if [ -z $1 ]; then
 	echo "Please provide board name as argunment"
-	exit -1
+	help
+fi
+
+IFACE=can0
+
+if [ ! -z $1 ]; then
+	IFACE=$2
+fi
+
+if [ ! -d /sys/class/net/$IFACE ]; then
+	echo "Interface $IFACE is not exist"
+	help
+fi
+
+if [ $(</sys/class/net/$IFACE/operstate) != up ]; then
+	echo "CAN interface $IFACE is not UP"
+	echo " You may want to bring up $IFACE first"
+	echo " ip link set $IFACE type can bitrate 500000 ; ifconfig $IFACE up"
+	help
 fi
 
 FW_FILE=deliver/$1/wideband_update.srec
 
 if [ ! -f $FW_FILE ]; then
 	echo "FW_FILE $FW_FILE not found"
-	exit -1
+	help
 fi
 
-# This script will try to flash/update RusEFI part of firmware over can0 interface.
+# This script will try to flash/update RusEFI part of firmware over can interface.
 #
-# You may want to bring can0 interface up first:
+# You may want to bring can interface up first. For can0:
 # sudo ip link set can0 type can bitrate 500000 ; sudo ifconfig can0 up
 # Sometimes (after communication errors) you may like to restart interface with:
 # make sure that you have termination resistor
 # sudo ifconfig can0 down; sudo ip link set can0 type can bitrate 500000 ; sudo ifconfig can0 up
 
-# sudo apt-get install can-utils
-# candump can0
-
-/sbin/ifconfig can0 | grep RUNNING
-
-#echo This script assumes that you are using can0 interface and it is ready, otherwise read comments inside
+#echo This script assumes that you are using $IFACE interface and it is ready, otherwise read comments inside
 #echo This script assumes that you have BootCommander somewhere in your PATH, otherwise read comments inside
-#BootCommander -t=xcp_can -d=can0 $FW_FILE
+#BootCommander -t=xcp_can -d=$IFACE $FW_FILE
 
 # OR
 # You can build it from sources with:
@@ -45,6 +67,7 @@ if [ ! -f ext/openblt/Host/BootCommander ]; then
 	echo "Please build BootCommander first!"
 	echo "Run: (cd ext/openblt/Host/Source/LibOpenBLT/ ; mkdir build ; cd build ; cmake .. ; make -j )"
 	echo "Run: (cd ext/openblt/Host/Source/BootCommander/ ; mkdir build ; cd build ; cmake .. ; make -j )"
+	help
 fi
 
-ext/openblt/Host/BootCommander -t=xcp_can -d=can0 $FW_FILE
+ext/openblt/Host/BootCommander -t=xcp_can -d=$IFACE $FW_FILE
