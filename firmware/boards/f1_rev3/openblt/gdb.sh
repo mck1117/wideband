09872e0d3ca62e1b91355b0da9fb6ae518e0f120
rusefillc <48498823+rusefillc@users.noreply.github.com>
2022-08-20 14:39:03 -0400

    F103 rev 3 (#136)
    * f1_rev3: add board support (mostly copy-paste)
    
    * f1_rev3: add to workflows
    
    * hal_mfs.mk - one way or another we will use MFS for sure in this repo
    
    * SPI means EGT and EGT means SPI
    
    * one step towards master
    
    Co-authored-by: Andrey Gusakov <dron0gus@gmail.com>
    Co-authored-by: rusefillc <sdfsdfqsf2334234234>

---
+++ firmware/boards/f1_rev3/openblt/gdb.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#../../toolchain/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-gdb $*
+#../../toolchain/gcc-arm-none-eabi-9-2019-q4-major/bin/arm-none-eabi-gdb $*
+
+arm-none-eabi-gdb $*
