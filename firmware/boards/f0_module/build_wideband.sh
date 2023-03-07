43def3f98257850f7820503f0984cda0e3c078d0
Matthew Kennedy <matthewkennedy@outlook.com>
2023-03-06 21:12:49 -0800

    output to deliver dir, so that ci picks it up

--- firmware/boards/f0_module/build_wideband.sh
+++ firmware/boards/f0_module/build_wideband.sh
@@ -9,12 +9,19 @@ cd bootloader
 # back out to the root
 cd ../../..
 
+BOARD=f0_module
+DELIVER_DIR=deliver/${BOARD}
+
 # delete the elf to force a re-link (it might not pick up the bootloader otherwise)
 rm -rf build/
 rm -f ../for_rusefi/wideband_image.h
 
+mkdir -p ${DELIVER_DIR}
+rm -rf ${DELIVER_DIR}/*
+
 # build app firmware!
-make -j12 BOARD=f0_module
+make -j12 BOARD=${BOARD}
+
 
 # Copy the bin without the bootloader (the image consumed by rusEfi has no bootloader on it)
 dd if=build/wideband.bin of=build/wideband_noboot_no_pad.bin skip=6144 bs=1
@@ -29,9 +36,12 @@ crc32 build/wideband_fullsize_nocrc.bin | xxd -r -p - > build/wideband_crc.bin
 cat build/wideband_fullsize_nocrc.bin build/wideband_crc.bin > build/wideband_image.bin
 
 # For good measure build a complete bin that includes the bootloader
-cat boards/f0_module/bootloader/build/wideband_bootloader.padded.bin build/wideband_image.bin > ../for_rusefi/wideband_image_with_bl.bin
+cat boards/f0_module/bootloader/build/wideband_bootloader.padded.bin build/wideband_image.bin > ${DELIVER_DIR}/wideband_image_with_bl.bin
 
 # Convert to a header file, and tack "static const " on the front of it
 xxd -i build/wideband_image.bin \
     | cat <(echo -n "static const ") - \
-    > ../for_rusefi/wideband_image.h
+    > ${DELIVER_DIR}/wideband_image.h
+
+cp ${DELIVER_DIR}/wideband_image_with_bl.bin ../for_rusefi/wideband_image_with_bl.bin
+cp ${DELIVER_DIR}/wideband_image.h ../for_rusefi/wideband_image.h
