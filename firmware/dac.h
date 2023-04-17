898fa20c2b89ffeb27407a65b914c3bc4535b1b7
rusefillc <48498823+rusefillc@users.noreply.github.com>
2023-04-17 17:11:34 -0400

    Dac helper (#226)
    * dac: move helpers to separate cpp and h
    
    (cherry picked from commit aee71931db269fc256ab43e1fb84b71c77e215d7)
    
    * dac: HAL_USE_DAC guards
    
    (cherry picked from commit 5517844e4d0f79c7cf8c5bb097c110541063bddf)
    
    ---------
    
    Co-authored-by: Andrey Gusakov <dron0gus@gmail.com>

---
+++ firmware/dac.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+
+/* for DACConfig */
+#include "hal.h"
+
+#if HAL_USE_DAC
+
+class Dac
+{
+public:
+    Dac(DACDriver& driver);
+
+    void Start(DACConfig& config);
+    void SetVoltage(int channel, float volage);
+    float GetLastVoltage(int channel);
+
+private:
+    DACDriver* const m_driver;
+    float m_voltageFloat[2];
+};
+
+#endif
