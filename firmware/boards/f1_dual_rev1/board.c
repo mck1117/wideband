6041f6eed5665d369ea8d0747698119081f53a39
Andrey Gusakov <dron0gus@gmail.com>
2023-04-18 12:00:40 +0300

    f1_dual_rev1: fix remaps
    -disable JTAG to free PA15, PB3, PB4
    -use SPI3 for EGT chips without remaping
    
    (cherry picked from commit 598f1aa0779ba6e90ccf2eb74445d074f2bfbc75)

--- firmware/boards/f1_dual_rev1/board.c
+++ firmware/boards/f1_dual_rev1/board.c
@@ -48,6 +48,6 @@ void __early_init(void) {
  * Board-specific initialization code.
  */
 void boardInit(void) {
-  AFIO->MAPR |= AFIO_MAPR_SPI1_REMAP |  /* remap SPI1 to PB3, PB4, PB5 and PA15 */
-                AFIO_MAPR_I2C1_REMAP;   /* remap I2C1 to PB8, PB9 */
+  AFIO->MAPR |= AFIO_MAPR_I2C1_REMAP |    /* remap I2C1 to PB8, PB9 */
+                AFIO_MAPR_SWJ_CFG_1;      /* JTAG-DP disabled, SW-DP enabled, PA13, PA14 are only used */
 }
