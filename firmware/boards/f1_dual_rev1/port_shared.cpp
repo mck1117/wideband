ebcf74377243ffda574830078be51c1de1dd697c
Andrey Gusakov <dron0gus@gmail.com>
2023-04-18 23:36:57 +0300

    f1_dual_rev1: fix can timings
    (cherry picked from commit 6437b3d92047adefe6534efaa57062bdda449d37)

--- firmware/boards/f1_dual_rev1/port_shared.cpp
+++ firmware/boards/f1_dual_rev1/port_shared.cpp
@@ -6,9 +6,9 @@ static const CANConfig canConfig500 =
 {
     CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
     /*
-     For 24MHz http://www.bittiming.can-wiki.info/ gives us Pre-scaler=3, Seq 1=13 and Seq 2=2. Subtract '1' for register values
+     For 36MHz http://www.bittiming.can-wiki.info/ gives us Pre-scaler=4, Seq 1=15 and Seq 2=2. Subtract '1' for register values
     */
-    CAN_BTR_SJW(0) | CAN_BTR_BRP(2)  | CAN_BTR_TS1(12) | CAN_BTR_TS2(1),
+    CAN_BTR_BRP(3) | CAN_BTR_TS1(14) | CAN_BTR_TS2(1) | CAN_BTR_SJW(0),
 };
 
 const CANConfig& GetCanConfig() {
