ef1c89a51df013830b083ba17fa3dad3c34ddb45
rusefillc <48498823+rusefillc@users.noreply.github.com>
2023-04-27 15:56:27 -0400

    Dual rev1 (#229)
    * f1_dual: revision 1
    
    (cherry picked from commit 855585fcf66d5ca8d1d549236c1c472bbfcaa356)
    
    * github: build f1_dual_rev1
    
    (cherry picked from commit 8ac01d43831b5231303a430cdf56fcabafe9c990)
    
    * pump_dac: support DAC as utput interface
    
    (cherry picked from commit 5240ec1a9ed88110e63785b1611223bb4f6a023c)
    
    ---------
    
    Co-authored-by: Andrey Gusakov <dron0gus@gmail.com>

---
+++ firmware/boards/f1_dual_rev1/wideband_board_config.h
@@ -0,0 +1,76 @@
+#pragma once
+
+// TS settings
+#define TS_SIGNATURE "rusEFI 2023.03.23.wideband_dual"
+
+// This board implements two channels
+#define AFR_CHANNELS 2
+#define EGT_CHANNELS 2
+
+// Fundamental board constants
+#define VCC_VOLTS (3.3f)
+#define HALF_VCC (VCC_VOLTS / 2)
+#define ADC_MAX_COUNT (4095)
+#define ADC_OVERSAMPLE 16
+
+// *******************************
+//    Nernst voltage & ESR sense
+// *******************************
+#define NERNST_INPUT_GAIN (1 / 3.15f)
+#define NERNST_INPUT_OFFSET (0.247)
+
+// *******************************
+//        Battery Sensing
+// *******************************
+// 100K + 10K divider
+#define BATTERY_INPUT_DIVIDER (10.0 / (10.0 + 100.0))
+#define BATTERY_FILTER_ALPHA (0.1f)
+
+// *******************************
+//     Heater low side Sensing
+// *******************************
+// 100K + 10K divider
+#define HEATER_INPUT_DIVIDER (10.0 / (10.0 + 100.0))
+
+// *******************************
+//        Vm output Sensing
+// *******************************
+// 100K + 10K divider
+#define VM_INPUT_DIVIDER (10.0 / (10.0 + 100.0))
+
+// *******************************
+//    Nernst voltage & ESR sense
+// *******************************
+#define VM_RESISTOR_VALUE (0)
+
+// *******************************
+//          AUX outputs
+// *******************************
+// OpAmp with 82K + 160K
+#define AUXOUT_GAIN         ((82.0 + 160.0) / 160.0)
+
+// *******************************
+// With heater constantly on we
+// are not able to measure Vbatt
+// through Heater-
+// *******************************
+// TODO: remove!
+#define HEATER_MAX_DUTY		(0.85)
+
+// *******************************
+//   TunerStudio Primary Port
+// *******************************
+#define TS_PRIMARY_SERIAL_PORT	SD1
+#define TS_PRIMARY_BAUDRATE		115200
+
+// TODO: add BT port on PC10, PC11 - UART4/USART3
+
+// *******************************
+//   MAX31855 spi port
+// *******************************
+#define EGT_SPI_DRIVER			(&SPID1)
+
+// Serial connection over BT module
+// enable BT module setup attempt
+#define BT_SERIAL_OVER_JDY33	TRUE
+#define BT_BROADCAST_NAME		"RusEFI WBO x2"
