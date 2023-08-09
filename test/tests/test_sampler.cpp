1897917dd7ceb86e6e1cd0ccaf3907578be4c55a
Matthew Kennedy <matthewkennedy@outlook.com>
2023-08-09 13:36:48 -0700

    dc sampler test

--- test/tests/test_sampler.cpp
+++ test/tests/test_sampler.cpp
@@ -14,7 +14,22 @@ int GetESRSupplyR()
     return 22000;
 }
 
-TEST(Sampler, Basic)
+TEST(Sampler, TestDcBehavior)
 {
     Sampler dut;
+
+    AnalogChannelResult data;
+    data.NernstVoltage = 0.45f;
+    data.PumpCurrentVoltage = 1.75f;
+    constexpr float virtualGroundVoltage = 1.65f;
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        dut.ApplySample(data, virtualGroundVoltage);
+    }
+
+    // not exactly 0 because of filtering
+    EXPECT_NEAR(0, dut.GetNernstAc(), 1e-3);
+    EXPECT_FLOAT_EQ(0.45f, dut.GetNernstDc());
+    EXPECT_NEAR(-0.0295, dut.GetPumpNominalCurrent(), 1e-3);
 }
