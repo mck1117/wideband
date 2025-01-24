7954cbd5a441a859e804c328519e33b7654a4c1d
Matthew Kennedy <matthewkennedy@outlook.com>
2025-01-23 16:40:32 -0800

    extract pid config type

--- firmware/pid.h
+++ firmware/pid.h
@@ -1,25 +1,26 @@
 #pragma once
 
+struct PidConfig
+{
+    float kP;
+    float kI;
+    float kD;
+    float clamp;
+    int periodMs;
+};
+
 class Pid
 {
 public:
-    Pid(float kP, float kI, float kD, float clamp, int periodMs) 
-        : m_period(periodMs / 1000.0f)
-        , m_kp(kP)
-        , m_ki(kI)
-        , m_kd(kD)
-        , m_clamp(clamp)
+    Pid(const PidConfig& config) 
+        : m_config(&config)
     {
     }
 
     float GetOutput(float setpoint, float observation);
 
 private:
-    const float m_period;
-    const float m_kp;
-    const float m_ki;
-    const float m_kd;
-    const float m_clamp;
+    const PidConfig* const m_config;
 
     float m_lastError = 0;
     float m_integrator = 0;
