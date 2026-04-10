700649091c1837f9647929578aa04957ea9d5226
Matthew Kennedy <matthewkennedy@outlook.com>
2026-04-10 13:35:16 -0700

    format util

--- firmware/util/byteswap.h
+++ firmware/util/byteswap.h
@@ -4,11 +4,10 @@
 
 static inline uint16_t SWAP_UINT16(uint16_t x)
 {
-	return ((x << 8) | (x >> 8));
+    return ((x << 8) | (x >> 8));
 }
 
 static inline uint32_t SWAP_UINT32(uint32_t x)
 {
-	return (((x >> 24) & 0x000000ff) | ((x <<  8) & 0x00ff0000) |
-			((x >>  8) & 0x0000ff00) | ((x << 24) & 0xff000000));
+    return (((x >> 24) & 0x000000ff) | ((x << 8) & 0x00ff0000) | ((x >> 8) & 0x0000ff00) | ((x << 24) & 0xff000000));
 }
--- firmware/util/thread_controller.h
+++ firmware/util/thread_controller.h
@@ -1,6 +1,6 @@
 /**
  *  @file thread_controller.h
- * 
+ *
  *  @date Jan 5, 2019
  *  @author Matthew Kennedy, (c) 2019
  */
@@ -9,13 +9,12 @@
 
 /**
  * @brief A base class for a controller that requires its own thread.
- * 
+ *
  * Inherit from ThreadController.  Implement ThreadTask with the logic required for your thread.
  * The template parameter specifies the size of the stack used for the thread.  (because we have to
  * allocate the stack at compile time, it has to be a template parameter instead of a normal parameter)
  */
-template <int TStackSize>
-class ThreadController
+template <int TStackSize> class ThreadController
 {
 private:
     THD_WORKING_AREA(m_threadstack, TStackSize);
@@ -41,7 +40,7 @@ protected:
     virtual void ThreadTask() = 0;
     thread_t* m_thread;
 
-	const char* const m_name;
+    const char* const m_name;
 
 public:
     ThreadController(const char* name, tprio_t priority)
@@ -55,8 +54,9 @@ public:
      */
     void Start()
     {
-        if (m_isStarted) {
-            //warning(CUSTOM_OBD_6003, "Tried to start thread %s but it was already running", m_name);
+        if (m_isStarted)
+        {
+            // warning(CUSTOM_OBD_6003, "Tried to start thread %s but it was already running", m_name);
             return;
         }
 
